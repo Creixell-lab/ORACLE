541ac6d05928eb04a9d9ab4b29a62ea1f4d918bd
PanoptoSalad <royalaceforces@gmail.com>
2025-12-15 21:43:20 +0000

    init py added core functions to be introduced piecewise later

--- src/oracle_epistasis/__init__.py
+++ src/oracle_epistasis/__init__.py
@@ -1,6 +1,34 @@
 # __init__.py
 
-from .pipeline import run_otacle_epistasis
+#from .pipeline import run_oracle_epistasis
 
 __version__ = "0.1.0"
 __author__ = "Mingxuan Jiang"
+
+from .io import add_position_columns, read_library_csv
+
+from .model import (
+    epistasis_code,
+    epistasis_value,
+    y_from_epi,
+    find_matching_wildcards,
+    string_indices,
+    learn_positionwise_map_strict,
+    decode_with_position_map,
+)
+
+
+from .pipeline import run_epistasis_pipeline
+
+__all__ = [
+    "read_library_csv",
+    "add_position_columns",
+    "epistasis_code",
+    "epistasis_value",
+    "y_from_epi",
+    "find_matching_wildcards",
+    "string_indices",
+    "run_epistasis_pipeline",
+    "learn_positionwise_map_strict",
+    "decode_with_position_map",
+]
